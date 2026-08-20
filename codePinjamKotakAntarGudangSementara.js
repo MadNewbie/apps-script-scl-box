8fb41b6a45810a24d7d45d79daef977ca7d8f168
Rachmad Yanuarianto <rachmad.y25@gmail.com>
2026-08-20 08:52:28 +0700

    menyelesaikan fitur pinjam kotak antar gudang sementara

---
+++ codePinjamKotakAntarGudangSementara.js
@@ -0,0 +1,37 @@
+function showPinjamKotakAntarGudang() {
+  var formPinjamKotakAntarGudangSementara = HtmlService
+    .createHtmlOutputFromFile('formPinjamKotakAntarGudangSementara')
+    .setWidth(1000)
+    .setHeight(750)
+
+    SpreadsheetApp.getUi()
+      .showModalDialog(formPinjamKotakAntarGudangSementara, 'Form Transfer Kotak Antar Gudang Sementara')
+}
+
+function saveDataPinjamKotakAntarGudangSementara(idKotakDipinjam, tujuanPeminjaman, gudangTujuan) {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet()
+  var sheetPinjamKotak = spreadsheet.getSheetByName('Riwayat Pinjam Kotak Antar Gudang Sementara')
+  var lrPinjamKotak = sheetPinjamKotak.getLastRow()
+
+  var sheetKotak = spreadsheet.getSheetByName('Kotak')
+  var lrKotak = sheetKotak.getLastRow()
+
+  var user = Session.getActiveUser().getEmail()
+  var date = new Date()
+
+  idKotakDipinjam.forEach(id => {
+    var dataRow = sheetKotak.getRange(2,1,lrKotak-1,11).getValues().findIndex((data)=>{return data[0]===id}) + 2
+    sheetPinjamKotak.getRange(lrPinjamKotak+1,1).setValue(sheetKotak.getRange(dataRow,1).getValue())
+    sheetPinjamKotak.getRange(lrPinjamKotak+1,2).setValue(sheetKotak.getRange(dataRow,5).getValue())
+    sheetPinjamKotak.getRange(lrPinjamKotak+1,3).setValue(tujuanPeminjaman)
+    sheetPinjamKotak.getRange(lrPinjamKotak+1,4).setValue(sheetKotak.getRange(dataRow,8).getValue())
+    sheetPinjamKotak.getRange(lrPinjamKotak+1,5).setValue(gudangTujuan)
+    sheetPinjamKotak.getRange(lrPinjamKotak+1,6).setValue('FALSE')
+    sheetPinjamKotak.getRange(lrPinjamKotak+1,7).setValue(user)
+    sheetPinjamKotak.getRange(lrPinjamKotak+1,8).setValue(Utilities.formatDate(date,"Asia/Bangkok","dd MMMM yyyy HH:mm:ss"))
+    sheetKotak.getRange(dataRow,8).setValue(gudangTujuan)
+    lrPinjamKotak = lrPinjamKotak + 1
+  });
+
+  return 'Berhasil mencatat peminjaman kotak'
+}
